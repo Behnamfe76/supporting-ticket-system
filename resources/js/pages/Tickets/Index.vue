4310314cbc2a8637e887cc65e71106240ea6b150
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-06-28 17:40:06 +0330

    feat: implement ticket creation and display functionality
    - Added a new Create.vue component for creating tickets with form validation and file upload support.
    - Integrated toast notifications for error handling and user feedback during ticket creation.
    - Updated Index.vue to include a button for navigating to the ticket creation page.
    - Introduced a Show.vue component for displaying individual ticket details with breadcrumb navigation.

--- resources/js/pages/Tickets/Index.vue
+++ resources/js/pages/Tickets/Index.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import Pagination from '@/components/ui/pagination/Pagination.vue';
+import { SimpleTable } from '@/components/ui/tables/simple-table';
+import SimpleTableSkeleton from '@/components/ui/tables/simple-table/SimpleTableSkeleton.vue';
+import { Tabs } from '@/components/ui/tabs';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Tabs } from '@/components/ui/tabs';
-import { SimpleTable } from '@/components/ui/tables/simple-table';
-import { Head } from '@inertiajs/vue3';
+import { Head, router } from '@inertiajs/vue3';
+import { Pen } from 'lucide-vue-next';
 import { reactive, ref } from 'vue';
-import SimpleTableSkeleton from '@/components/ui/tables/simple-table/SimpleTableSkeleton.vue';
-import Pagination from '@/components/ui/pagination/Pagination.vue';
 
 const loading = ref(false);
 const tabs = reactive([
     { name: 'All', href: '#', count: 52, current: true },
     { name: 'Open', href: '#', count: 6, current: false },
     { name: 'In Progress', href: '#', count: 4, current: false },
-    { name: 'Closed', href: '#', current: false }
+    { name: 'Closed', href: '#', current: false },
 ]);
 
 const tableData = reactive({
@@ -24,47 +26,60 @@ const tableData = reactive({
                 'Lindsay Walton',
                 'Front-end Developer',
                 'Optimization',
-                { value: 'Active', badge: true, badgeType: 'success' },
+                {
+                    value: 'Active',
+                    badge: true,
+                    badgeType: 'success',
+                },
                 'Member',
             ],
             operations: [
                 { label: 'Edit', action: 'edit', icon: 'Pen' },
-                { label: 'Delete', action: 'delete', icon: 'Trash2' }
-            ]
+                { label: 'Delete', action: 'delete', icon: 'Trash2' },
+            ],
         },
         {
             cells: [
                 'Courtney Henry',
                 'Designer',
                 'UX',
-                { value: 'Inactive', badge: true, badgeType: 'danger' },
+                {
+                    value: 'Inactive',
+                    badge: true,
+                    badgeType: 'danger',
+                },
                 'Admin',
             ],
-            operations: [
-                { label: 'Edit', action: 'edit', icon: 'Pen' }
-            ]
-        }
-    ]
+            operations: [{ label: 'Edit', action: 'edit', icon: 'Pen' }],
+        },
+    ],
 });
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Tickets',
-        href: '/tickets'
-    }
+        href: '/tickets',
+    },
 ];
 
-function handleOperationClick({ action, rowIdx }: { action: string, rowIdx: number }) {
+function handleOperationClick({ action, rowIdx }: { action: string; rowIdx: number }) {
     alert(`Operation: ${action} on row ${rowIdx + 1}`);
 }
 </script>
 
 <template>
-
     <Head title="Dashboard" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <!-- Header -->
+            <Button @click="router.get(route('tickets.create'))" class="group flex cursor-pointer items-center overflow-hidden px-4 py-2 transition-all duration-300">
+                <span class="transition-all duration-300">Create A Ticket</span>
+                <Pen
+                    class="-translate-x-2 opacity-0 transition-all duration-300 group-hover:ml-2 group-hover:translate-x-0 group-hover:opacity-100"
+                />
+            </Button>
+
             <!-- Tabs -->
             <Tabs :tabs="tabs" />
 
@@ -73,7 +88,7 @@ function handleOperationClick({ action, rowIdx }: { action: string, rowIdx: numb
             <SimpleTableSkeleton v-else :rows="10" />
 
             <!-- Pagination -->
-             <Pagination />
+            <Pagination />
         </div>
     </AppLayout>
 </template>
---
+++ resources/js/pages/Tickets/Show.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Tickets',
+        href: '/tickets',
+    },
+    {
+        title: 'Show',
+        href: '/tickets/show',
+    },
+];
+
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            show
+        </div>
+    </AppLayout>
+</template>
