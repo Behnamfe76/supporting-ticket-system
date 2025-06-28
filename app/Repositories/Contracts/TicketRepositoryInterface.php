7f0e764eff5f9d132445832950e121201e475684
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-06-28 21:53:34 +0330

    feat: add user ticket retrieval and counting methods to TicketRepository
    - Implemented getUserTickets method to paginate and filter tickets based on user status.
    - Added ticketCounts method to retrieve the count of tickets grouped by status for the authenticated user.
    - Updated TicketRepositoryInterface to include new method signatures for improved contract adherence.

--- app/Repositories/Contracts/TicketRepositoryInterface.php
+++ app/Repositories/Contracts/TicketRepositoryInterface.php
@@ -4,12 +4,27 @@ namespace App\Repositories\Contracts;
 
 use App\Data\TicketData;
 use App\Models\Ticket;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 interface TicketRepositoryInterface
 {
+    /**
+     * @param Request $request
+     * @return LengthAwarePaginator
+     */
+    public function getUserTickets(Request $request): LengthAwarePaginator;
+
     /**
      * @param TicketData $data
      * @return Ticket
      */
     public function create(TicketData $data): Ticket;
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function ticketCounts(Request $request): mixed;
+
 }
--- app/Repositories/TicketRepository.php
+++ app/Repositories/TicketRepository.php
@@ -2,12 +2,55 @@
 
 namespace App\Repositories;
 
-use App\Data\TicketData;
 use App\Models\Ticket;
+use App\Data\TicketData;
+use App\Enums\TicketStatus;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Pagination\LengthAwarePaginator;
 use App\Repositories\Contracts\TicketRepositoryInterface;
 
 class TicketRepository implements TicketRepositoryInterface
 {
+    /**
+     * @param Request $request
+     * @return LengthAwarePaginator
+     */
+    public function getUserTickets(Request $request): LengthAwarePaginator
+    {
+        $userId = $request->user()->id;
+
+        return Ticket::select('subject', 'priority', 'status', 'created_at')
+        ->when($request->has('status'), function ($query) use ($request) {
+                $status = TicketStatus::fromString($request->status);
+
+                if($status instanceof TicketStatus){
+                    $query->where('status', $status->value);
+                }
+            })
+            ->whereHas('replies', function ($query) use ($userId) {
+                $query->where('author_id', $userId);
+            })
+            ->paginate(10);
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function ticketCounts(Request $request): mixed
+    {
+        $userId = $request->user()->id;
+
+        return Ticket::whereHas('replies', function ($query) use ($userId) {
+            $query->where('author_id', $userId);
+        })
+            ->select('status', DB::raw('count(*) as count'))
+            ->groupBy('status')
+            ->get();
+    }
+
     /**
      * @param TicketData $data
      * @return Ticket
