72b08868a5187eb08c048057ec4708923212b910
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-06-29 20:35:44 +0330

    feat: add admin layout components for improved UI structure
    - Introduced AdminLayout, AdminHeaderLayout, and AdminSidebarLayout components to enhance the admin interface.
    - Implemented breadcrumb support in each layout for better navigation.
    - Structured the layouts to include sidebar and header components, improving overall user experience.

---
+++ resources/js/layouts/admin/AdminSidebarLayout.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import AppContent from '@/components/AppContent.vue';
+import AppShell from '@/components/AppShell.vue';
+import AppSidebar from '@/components/AppSidebar.vue';
+import AppSidebarHeader from '@/components/AppSidebarHeader.vue';
+import type { BreadcrumbItemType, NavItem } from '@/types';
+import { LayoutGrid, Mail } from 'lucide-vue-next';
+
+interface Props {
+    breadcrumbs?: BreadcrumbItemType[];
+}
+
+const mainNavItems: NavItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/admin/dashboard',
+        icon: LayoutGrid,
+    },{
+        title: 'Tickets',
+        href: '/admin/tickets',
+        icon: Mail,
+    },
+];
+
+withDefaults(defineProps<Props>(), {
+    breadcrumbs: () => [],
+});
+</script>
+
+<template>
+    <AppShell variant="sidebar">
+        <AppSidebar :mainNavItems="mainNavItems" />
+        <AppContent variant="sidebar">
+            <AppSidebarHeader :breadcrumbs="breadcrumbs" />
+            <slot />
+        </AppContent>
+    </AppShell>
+</template>
