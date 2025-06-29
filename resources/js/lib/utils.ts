540f57d2012560a28ca6b7930aea88b79d1de6ac
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-06-29 14:23:10 +0330

    feat: add utility functions for string manipulation and status styling
    - Implemented capitalize function to format strings by capitalizing the first letter.
    - Added getImageName function to extract the image name from a given URL.
    - Created generateStatusStyles function to return corresponding styles based on ticket status, enhancing UI consistency.

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,25 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function capitalize(str: string) {
+    if (!str) return '';
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+export const getImageName = (str: string) => str?.split('/').pop() || '';
+
+export function generateStatusStyles(status: string): string {
+    switch (status.toLowerCase()) {
+        case 'open':
+            return 'text-green-700 bg-green-200';
+        case 'in_progress':
+            return 'text-yellow-700 bg-yellow-200';
+        case 'closed':
+            return 'text-red-700 bg-red-200';
+        case 'resolved':
+            return 'text-indigo-700 bg-indigo-200';
+        default:
+            return 'text-blue-700 bg-blue-300';
+    }
+}
