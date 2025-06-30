2564d3541f54b738097a59f181ae9198bcf520a9
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-06-30 20:38:29 +0330

    feat: implement store and update methods in UserService
    - Added store method to handle user creation with transaction management and role assignment.
    - Implemented update method to modify existing user details.
    - Updated UserServiceInterface to include new method signatures for store and update operations, enhancing user management capabilities.

--- app/Services/Contracts/UserServiceInterface.php
+++ app/Services/Contracts/UserServiceInterface.php
@@ -4,8 +4,10 @@ namespace App\Services\Contracts;
 
 use App\Data\ReplyData;
 use App\Data\TicketData;
+use App\Data\UserData;
 use App\Http\Requests\Tickets\StoreReplyRequest;
 use App\Models\Ticket;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -17,4 +19,17 @@ interface UserServiceInterface
      * @return mixed
      */
     public function index(): mixed;
+
+    /**
+     * @param UserData $data
+     * @return User|Exception
+     */
+    public function store(UserData $data): User|Exception;
+
+    /**
+     * @param UserData $data
+     * @param User $user
+     * @return bool
+     */
+    public function update(UserData $data, User $user): bool;
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,9 +2,14 @@
 
 namespace App\Services;
 
+use App\Data\UserData;
+use App\Models\User;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Services\Contracts\UserServiceInterface;
 use App\Traits\MediaExtraInteractions;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class UserService implements UserServiceInterface
 {
@@ -13,7 +18,8 @@ class UserService implements UserServiceInterface
     public function __construct(
         protected UserRepositoryInterface $userRepository,
     )
-    {}
+    {
+    }
 
     /**
      * @return mixed
@@ -21,5 +27,37 @@ class UserService implements UserServiceInterface
     public function index(): mixed
     {
         return $this->userRepository->Index();
+    }
+
+
+    /**
+     * @param UserData $data
+     * @return User|Exception
+     */
+    public function store(UserData $data): User|Exception
+    {
+        try {
+            DB::beginTransaction();
+            $user = $this->userRepository->store($data);
+            $user->assignRole('user');
+            DB::commit();
+
+            return $user;
+        }catch(Exception $e){
+            DB::rollBack();
+            Log::error($e->getMessage());
+
+            return $e;
+        }
+    }
+
+    /**
+     * @param UserData $data
+     * @param User $user
+     * @return bool
+     */
+    public function update(UserData $data, User $user): bool
+    {
+        return $this->userRepository->update($data, $user);
     }
 }
