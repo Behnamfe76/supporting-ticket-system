e881d7f9f708a2787983677716784d78cdba246e
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-06-30 15:11:37 +0330

    feat: bind UserService and UserRepository in AppServiceProvider
    - Registered UserService and UserRepository in the AppServiceProvider to facilitate user management operations.
    - Updated the service provider to include bindings for UserServiceInterface and UserRepositoryInterface, enhancing the application's dependency injection capabilities.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,14 @@ namespace App\Providers;
 
 use App\Repositories\Contracts\ReplyRepositoryInterface;
 use App\Repositories\Contracts\TicketRepositoryInterface;
+use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\ReplyRepository;
 use App\Repositories\TicketRepository;
+use App\Repositories\UserRepository;
 use App\Services\Contracts\TicketServiceInterface;
+use App\Services\Contracts\UserServiceInterface;
 use App\Services\TicketService;
+use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(ReplyRepositoryInterface::class, ReplyRepository::class);
         $this->app->bind(TicketRepositoryInterface::class, TicketRepository::class);
         $this->app->bind(TicketServiceInterface::class, TicketService::class);
+        $this->app->bind(UserServiceInterface::class, UserService::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
     }
 
     /**
